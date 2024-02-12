5253b3beb9afb74c42876928c5dae5a2570535b6
João Pedro Cordeiro <jpc5626@gmail.com>
2024-02-12 15:37:50 -0300

    feat: final adjustmets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,8 +42,8 @@
 
 		<div class="buttons">
 			{#if state === 'paused'}
-				<button>resume</button>
-				<button>quit</button>
+				<button on:click={() => game.resume()}>resume</button>
+				<button on:click={() => (state = 'waiting')}> quit </button>
 			{:else}
 				{#each levels as level}
 					<button
